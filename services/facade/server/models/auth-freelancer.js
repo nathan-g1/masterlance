797e7501ffdcd3079028a0417717f7b69aabf31b
ermias111 <ermimichael@gmail.com>
2020-03-23 07:47:00 -0400

    (facade): create /register end point

--- services/facade/server/models/auth-freelancer.js
+++ services/facade/server/models/auth-freelancer.js
@@ -1,5 +1,22 @@
 'use strict';
 
-module.exports = function(Authfreelancer) {
+module.exports = function (Authfreelancer) {
 
+    /**
+     * register as freelancer
+     * @param {object} credentials 
+     * @param {Function(Error, object)} callback
+     */
+
+    Authfreelancer.register = function (credentials, callback) {
+        Authfreelancer.Freelancer_create({
+            data: credentials
+        }, (err, result) => {
+            if (err)
+                callback(err.obj.error, null)
+            else
+                callback(null, result.get('obj'))
+        });
+
+    };
 };
