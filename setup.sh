c57ea0c9caf614daad6d8944ae1db9e66c83ae71
Tibebejs <Tibebes.js@gmail.com>
2020-12-15 01:51:47 +0300

    :construction: update the setup file (fix)

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
-cd services/auth && npm i;
-cd services/core && npm i;
-cd services/facade && npm i;
-cd services/transaction && npm i;
+cd ./services/auth && npm i;
+cd ../core && npm i;
+cd ../facade && npm i;
+cd ../transaction && npm i;
 
